f1d327caccd545b56ed09f9ada86fc5fa90ba606
Josue516 <josue516x@gmail.com>
2026-06-17 14:15:42 -0500

    Cambios auth

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**",
+                "/api/health").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(firebaseAuthFilter, UsernamePasswordAuthenticationFilter.class);
