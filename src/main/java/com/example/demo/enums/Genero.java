455774d2d2d8bdcfa5dbfdbb9a00047b0d22b763
Josue516 <josue516x@gmail.com>
2026-06-19 13:06:39 -0500

    Generos

--- src/main/java/com/example/demo/enums/Genero.java
+++ src/main/java/com/example/demo/enums/Genero.java
@@ -1,19 +1,14 @@
 package com.example.demo.enums;
 
 public enum Genero {
-    ACCION,
-    ANIMACION,
-    AVENTURA,
-    CIENCIA_FICCION,
-    COMEDIA,
-    CRIMEN,
-    DRAMA,
-    FANTASIA,
-    HORROR,
-    MISTERIO,
-    MUSICAL,
-    ROMANCE,
-    SUSPENSO,
-    TERROR,
-    THRILLER
+	ACCION,
+	ANIMACION,
+	AVENTURA,
+	CIENCIA_FICCION,
+	COMEDIA,
+	DRAMA,
+	MISTERIO,
+	ROMANCE,
+	SUSPENSO,
+	TERROR
 }
