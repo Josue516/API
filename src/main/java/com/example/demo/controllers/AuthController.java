45266f3e02e1efdec266971300d94daa20893408
Josue516 <josue516x@gmail.com>
2026-06-20 01:06:20 -0500

    Corrigiendo bug

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -24,6 +24,7 @@ public class AuthController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
     private UsuarioService usuarioService;
 
     @PostMapping("/login")
